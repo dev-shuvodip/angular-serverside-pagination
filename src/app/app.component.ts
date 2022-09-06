6498a997f27c525babb5987588220ba4dac79a79
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-09-06 14:02:16 +0530

    added more page size options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements AfterViewInit {
   page!: number;
   limit!: number;
 
-  pageSizeOptions: number[] = [5, 10];
+  pageSizeOptions: number[] = [5, 10, 20, 25, 50];
   pageEvent!: PageEvent;
   isLoadingResults = true;
   displayedColumns: string[] = ['id', 'title', 'firstName', 'lastName'];
